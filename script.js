02933b9c53039c5120a90d09d73bdcf1957512c3
Sumnima B <sumnimabhandari@gmail.com>
2020-04-29 11:05:32 -0700

    Added the logic for localstorage saving and getting data back

--- script.js
+++ script.js
@@ -2,55 +2,61 @@
 var currentDayEl = $("#currentDay");
 var calendarHour = $(".hour");
 var today = moment();
+var storedValues = [];
 
 currentDayEl.text(today.format("LL"));
    
-   var testArray = ["nine","ten","eleven","twelve","one","two","three","four","five"];
-   var testRows = ["row1","row2","row3","row4","row5","row6","row7","row8","row9"];
-   var currentTime = (parseInt((today.format("LT"))))
-    // var labelTime = (parseInt ($("label#ten").text())); -- This code works
+var testArray = ["nine","ten","eleven","twelve","one","two","three","four","five"];
+var testRows = ["row1","row2","row3","row4","row5","row6","row7","row8","row9"];
+var taskArray = ["tasknine","taskten","taskeleven","tasktwelve","taskone","tasktwo","taskthree","taskfour","taskfive"];
+var currentTime = (parseInt((today.format("LT"))))
+
+function timeCheck(){
     
-    for (let i = 0; i < testArray.length; i++) {
-        
-        if((testArray[i] === "one") || (testArray[i] === "two") || (testArray[i] === "three") ||(testArray[i] === "four") ||(testArray[i] === "five")) {
-            var labelTime = (parseInt ($("label#"+ testArray[i]).text())) + 12; 
-        } else {
-            var labelTime = (parseInt ($("label#"+ testArray[i]).text())); 
-        }
-        var presentRow = $("textarea#" + testRows[i]);
-        console.log(labelTime)
-        console.log(currentTime)
-        if(currentTime === labelTime) {
-            presentRow.addClass("present");
-        } 
-        if ((currentTime > labelTime)){
-            presentRow.addClass("past");
-        } 
-        if ((currentTime < labelTime)){
-            presentRow.addClass("future");
-        } 
+for (let i = 0; i < testArray.length; i++) {  
+    if((testArray[i] === "one") || (testArray[i] === "two") || (testArray[i] === "three") ||(testArray[i] === "four") ||(testArray[i] === "five")) {
+        var labelTime = (parseInt ($("label#"+ testArray[i]).text())) + 12; 
+    } else {
+        var labelTime = (parseInt ($("label#"+ testArray[i]).text())); 
     }
-    
+    var presentRow = $("textarea#" + testRows[i]);
+    if(currentTime === labelTime) {
+        presentRow.addClass("present");
+    } 
+    if ((currentTime > labelTime)){
+        presentRow.addClass("past");
+    } 
+    if ((currentTime < labelTime)){
+        presentRow.addClass("future");
+    } 
+}
+}
 
-
-getValueFromLocalStorage()
-
+getValueFromLocalStorage();
+timeCheck();
 
 buttonEl = $(".saveBtn")
-buttonEl.on("click", function(e)
-{
-    var inputText = $("textarea#row1").val();
-    localStorage.setItem("task", JSON.stringify(inputText));
+buttonEl.on("click", saveDataToLocalStorage)
+
+
+
+function saveDataToLocalStorage(){
+    for (var i = 0; i < testRows.length; i++) {
+        var inputText = $("textarea#" + testRows[i]).val();
+        localStorage.setItem(taskArray[i], JSON.stringify(inputText));
+        
+    }
     
-});
+}
 
 
 function getValueFromLocalStorage(){
-    var getData = JSON.parse(localStorage.getItem("task"));
+    for (var i = 0; i < testRows.length; i++) {
+    var getData = JSON.parse(localStorage.getItem(taskArray[i]));
    
     if (getData !== null) {
-        $("textarea#row1").val(getData);
-
+        $("textarea#" + testRows[i]).val(getData);
     }
+}
 }
 
