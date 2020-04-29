ed4f0bf7d37ccc37d177efd5c621bfd6011b818a
Sumnima B <sumnimabhandari@gmail.com>
2020-04-28 22:56:49 -0700

    Fixed the color display for past, present and future classes

--- script.js
+++ script.js
@@ -4,28 +4,33 @@ var calendarHour = $(".hour");
 var today = moment();
 
 currentDayEl.text(today.format("LL"));
-
-//var timeArray = ["09AM","10AM","11AM","12PM"];
-
-    var labelTime = (parseInt(calendarHour.text().slice(0,2)))
-
-    var currentTime = (parseInt((today.format("HH"))))
-
-    var presentRow = $("#row");
-    var presentRow = $(".description")
-    if(currentTime === labelTime) {
-        presentRow.addClass("present");
-    } 
-    if ((currentTime > labelTime)){
-        presentRow.addClass("past");
-    } 
-    if ((currentTime < labelTime)){
-        presentRow.addClass("future");
-    }
-
-
    
-
+   var testArray = ["nine","ten","eleven","twelve","one","two","three","four","five"];
+   var testRows = ["row1","row2","row3","row4","row5","row6","row7","row8","row9"];
+   var currentTime = (parseInt((today.format("LT"))))
+    // var labelTime = (parseInt ($("label#ten").text())); -- This code works
+    
+    for (let i = 0; i < testArray.length; i++) {
+        
+        if((testArray[i] === "one") || (testArray[i] === "two") || (testArray[i] === "three") ||(testArray[i] === "four") ||(testArray[i] === "five")) {
+            var labelTime = (parseInt ($("label#"+ testArray[i]).text())) + 12; 
+        } else {
+            var labelTime = (parseInt ($("label#"+ testArray[i]).text())); 
+        }
+        var presentRow = $("textarea#" + testRows[i]);
+        console.log(labelTime)
+        console.log(currentTime)
+        if(currentTime === labelTime) {
+            presentRow.addClass("present");
+        } 
+        if ((currentTime > labelTime)){
+            presentRow.addClass("past");
+        } 
+        if ((currentTime < labelTime)){
+            presentRow.addClass("future");
+        } 
+    }
+    
 
 
 getValueFromLocalStorage()
@@ -34,11 +39,8 @@ getValueFromLocalStorage()
 buttonEl = $(".saveBtn")
 buttonEl.on("click", function(e)
 {
-    var inputText = $("textarea#row").val();
+    var inputText = $("textarea#row1").val();
     localStorage.setItem("task", JSON.stringify(inputText));
-
-
-
     
 });
 
@@ -47,17 +49,8 @@ function getValueFromLocalStorage(){
     var getData = JSON.parse(localStorage.getItem("task"));
    
     if (getData !== null) {
-        $("textarea#row").val(getData);
+        $("textarea#row1").val(getData);
 
     }
 }
 
-// function timeConvert(num)
-//  { 
-//   var hours = Math.floor(num / 60);  
-//   var minutes = num % 60;
-//   return hours + ":" + minutes;         
-// }
-//  var m = timeConvert(calendarHour.text())
-//  console.log(m)
-//  console.log(calendarHour.text())
