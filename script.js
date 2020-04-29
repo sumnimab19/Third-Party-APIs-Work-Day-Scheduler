cc891ff35e8349d8fa58abf963293fc4b998fdd3
Sumnima B <sumnimabhandari@gmail.com>
2020-04-29 11:38:28 -0700

    Added comments to the code and updated variable names to meaningful names

--- script.js
+++ script.js
@@ -1,62 +1,60 @@
 
 var currentDayEl = $("#currentDay");
-var calendarHour = $(".hour");
 var today = moment();
 var storedValues = [];
 
 currentDayEl.text(today.format("LL"));
    
-var testArray = ["nine","ten","eleven","twelve","one","two","three","four","five"];
-var testRows = ["row1","row2","row3","row4","row5","row6","row7","row8","row9"];
+var labelArray = ["nine","ten","eleven","twelve","one","two","three","four","five"];
+var inputRows = ["row1","row2","row3","row4","row5","row6","row7","row8","row9"];
 var taskArray = ["tasknine","taskten","taskeleven","tasktwelve","taskone","tasktwo","taskthree","taskfour","taskfive"];
 var currentTime = (parseInt((today.format("LT"))))
-
+ 
+// This function compares the current time with calendar display label time and adds class 'past', 'present','future' to show different colors dynamically
 function timeCheck(){
     
-for (let i = 0; i < testArray.length; i++) {  
-    if((testArray[i] === "one") || (testArray[i] === "two") || (testArray[i] === "three") ||(testArray[i] === "four") ||(testArray[i] === "five")) {
-        var labelTime = (parseInt ($("label#"+ testArray[i]).text())) + 12; 
-    } else {
-        var labelTime = (parseInt ($("label#"+ testArray[i]).text())); 
+    for (let i = 0; i < labelArray.length; i++) {  
+        if((labelArray[i] === "one") || (labelArray[i] === "two") || (labelArray[i] === "three") ||(labelArray[i] === "four") ||(labelArray[i] === "five")) {
+            var labelTime = (parseInt ($("label#"+ labelArray[i]).text())) + 12; 
+        } else {
+            var labelTime = (parseInt ($("label#"+ labelArray[i]).text())); 
+        }
+        var presentRow = $("textarea#" + inputRows[i]);
+        if(currentTime === labelTime) {
+            presentRow.addClass("present");
+        } 
+        if ((currentTime > labelTime)){
+            presentRow.addClass("past");
+        } 
+        if ((currentTime < labelTime)){
+            presentRow.addClass("future");
+        } 
     }
-    var presentRow = $("textarea#" + testRows[i]);
-    if(currentTime === labelTime) {
-        presentRow.addClass("present");
-    } 
-    if ((currentTime > labelTime)){
-        presentRow.addClass("past");
-    } 
-    if ((currentTime < labelTime)){
-        presentRow.addClass("future");
-    } 
-}
 }
 
 getValueFromLocalStorage();
 timeCheck();
 
+// Added event listener to Save buttons
 buttonEl = $(".saveBtn")
 buttonEl.on("click", saveDataToLocalStorage)
 
-
-
+// This function saves input data to localstorage
 function saveDataToLocalStorage(){
-    for (var i = 0; i < testRows.length; i++) {
-        var inputText = $("textarea#" + testRows[i]).val();
+    for (var i = 0; i < inputRows.length; i++) {
+        var inputText = $("textarea#" + inputRows[i]).val();
         localStorage.setItem(taskArray[i], JSON.stringify(inputText));
-        
     }
-    
 }
 
-
+// This function pulls data from local storage
 function getValueFromLocalStorage(){
-    for (var i = 0; i < testRows.length; i++) {
-    var getData = JSON.parse(localStorage.getItem(taskArray[i]));
+    for (var i = 0; i < inputRows.length; i++) {
+        var getData = JSON.parse(localStorage.getItem(taskArray[i]));
    
-    if (getData !== null) {
-        $("textarea#" + testRows[i]).val(getData);
+        if (getData !== null) {
+            $("textarea#" + inputRows[i]).val(getData);
+        }
     }
-}
 }
 
