6612ce62702d7c77baeced4f71ace45fd9147d4d
Sumnima B <sumnimabhandari@gmail.com>
2020-04-28 20:22:06 -0700

    Changes variable names to more meaningful names

--- script.js
+++ script.js
@@ -5,24 +5,28 @@ var today = moment();
 
 currentDayEl.text(today.format("LL"));
 
-var currentTime = today.format("LT");
+//var timeArray = ["09AM","10AM","11AM","12PM"];
 
-var x = (parseInt(calendarHour.text().slice(1,2)))
+    var labelTime = (parseInt(calendarHour.text().slice(0,2)))
+
+    var currentTime = (parseInt((today.format("HH"))))
+
+    var presentRow = $("#row");
+    var presentRow = $(".description")
+    if(currentTime === labelTime) {
+        presentRow.addClass("present");
+    } 
+    if ((currentTime > labelTime)){
+        presentRow.addClass("past");
+    } 
+    if ((currentTime < labelTime)){
+        presentRow.addClass("future");
+    }
 
 
-var y=(parseInt(currentTime.slice(0,1)))
-// console.log(y)
-// console.log(x)
-var presentRow = $("#row");
-if(y === x) {
-    presentRow.addClass("present");
-} 
-if ((y > x)){
-    presentRow.addClass("past");
-} 
-if ((y < x)){
-    presentRow.addClass("future");
-}
+   
+
+
 
 getValueFromLocalStorage()
 
@@ -41,10 +45,19 @@ buttonEl.on("click", function(e)
 
 function getValueFromLocalStorage(){
     var getData = JSON.parse(localStorage.getItem("task"));
-    console.log(getData)
-    
+   
     if (getData !== null) {
         $("textarea#row").val(getData);
 
     }
 }
+
+// function timeConvert(num)
+//  { 
+//   var hours = Math.floor(num / 60);  
+//   var minutes = num % 60;
+//   return hours + ":" + minutes;         
+// }
+//  var m = timeConvert(calendarHour.text())
+//  console.log(m)
+//  console.log(calendarHour.text())
